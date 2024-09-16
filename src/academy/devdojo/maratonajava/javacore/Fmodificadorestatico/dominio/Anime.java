40b539fc4ec3280812d6598c363e36dec361a999
Rafael Vieira Ferreira <161779899+rafaelviefe@users.noreply.github.com>
2024-09-16 16:41:31 -0300

    modificador estatico

---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/dominio/Anime.java
@@ -0,0 +1,39 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.dominio;
+
+public class Anime {
+    private String nome;
+    private static int[] episodios;
+    static {
+        System.out.println("Deladinho");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i+1;
+        }
+    }
+    static {
+        System.out.println("Deladinho2");
+    }
+    static {
+        System.out.println("Deladinho1");
+    }
+    {
+        System.out.println("nao deladinho");
+    }
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public Anime() {
+        for (int episodio: Anime.episodios) {
+            System.out.print(episodio + " ");
+        }
+        System.out.println();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public int[] getEpisodios() {
+        return episodios;
+    }
+}
